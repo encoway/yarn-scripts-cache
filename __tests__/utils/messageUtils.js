1277583660a8ea23388020e3222577c4de6de263
Roman Gischkowski <--global>
2024-02-02 12:56:16 +0100

    Fix texts: Yarn 4 changed the console output

--- __tests__/utils/messageUtils.js
+++ __tests__/utils/messageUtils.js
@@ -1,14 +1,14 @@
 
 function buildCacheUpdateMessage(packageName) {
-    return new RegExp(`➤ YN0000: \\[@rgischk\\/${packageName}\\]: ➤ YN0000: ┌ Updating script execution result cache\n➤ YN0000: \\[@rgischk\\/${packageName}\\]: ➤ YN0000: └ Completed`)
+    return new RegExp(`\\[@rgischk\\/${packageName}\\]: ➤ YN0000: ┌ Updating script execution result cache\n\\[@rgischk\\/${packageName}\\]: ➤ YN0000: └ Completed`)
 }
 
 function buildCacheReadMessage(packageName) {
-    return new RegExp(`➤ YN0000: \\[@rgischk\\/${packageName}\\]: ➤ YN0000: Script execution result was restored from file cache! Created .+ by [^\n]+\n`)
+    return new RegExp(`\\[@rgischk\\/${packageName}\\]: ➤ YN0000: Script execution result was restored from file cache! Created .+ by [^\n]+\n`)
 }
 
 function buildGreetingMessage(packageName, subject = "World") {
-    return new RegExp(`➤ YN0000: \\[@rgischk\\/${packageName}\\]: \\w+ ${subject}!`)
+    return new RegExp(`\\[@rgischk\\/${packageName}\\]: \\w+ ${subject}!`)
 }
 
 exports.buildCacheUpdateMessage = buildCacheUpdateMessage
